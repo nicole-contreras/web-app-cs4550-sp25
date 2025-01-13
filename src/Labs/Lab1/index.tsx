8e01f0e26bfc35fbf01ed4277b2a24ebfddfbb23
nicole-contreras <nicolecontreras9@icloud.com>
2025-01-13 12:23:52 -0500

    Introducing Headings

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,3 +1,9 @@
 export default function Lab2() {
-    return <h2>Lab 2</h2>
+    return (
+        <div>
+            <h2>Lab 2</h2>
+            <h3>Headings</h3>
+            <p>Headings allow you to break up the content into multiple different parts</p>
+        </div>
+    );
 }
