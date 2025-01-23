0b39a93bfc9cc0a560bc252afd73014d5acca2ea
nicole-contreras <nicolecontreras9@icloud.com>
2025-01-22 20:52:54 -0500

    Update github url and name

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kambaz">Kambaz</Link></li>
+        <li><a href="https://github.com/nicole-contreras/web-app-cs4550-sp25" id="wd-github">View the project repository on GitHub</a></li>
       </ul>
     );
   }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -9,6 +9,7 @@ export default function Labs() {
     return (
         <div>
             <h1>Labs</h1>
+            <h2>Nicole Contreras</h2>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
