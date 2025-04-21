a4104a91b29b5ff8c53418644d17bc9deb21b199
nicole-contreras <nicolecontreras9@icloud.com>
2025-04-21 19:33:42 -0400

    Enrollment Routes

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -10,6 +10,7 @@ import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
+import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 
 const app = express();
 
@@ -59,6 +60,7 @@ UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+EnrollmentRoutes(app);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
